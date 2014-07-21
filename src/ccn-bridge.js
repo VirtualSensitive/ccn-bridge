206dc98111a3bcd8b1937d80f3daf515b97d02d0
Flo-Schield-Bobby <florent.schildknecht@gmail.com>
2014-07-21 20:24:17 +0200

    Remove translate event --- add first aliases

--- src/ccn-bridge.js
+++ src/ccn-bridge.js
@@ -19,80 +19,22 @@ var CCNBridge = (function () {
       listeners,
       getListeners,
       on,
-      off;
+      onRotate,
+      onZoom,
+      off,
+      offRotate,
+      offZoom,
+      removeAllListeners,
+      trigger;
 
   events = {
-    'translate': 'translation',
     'rotate': 'rotation',
     'zoom': 'zoom'
   };
 
   listeners = {
-    'translation': [],
     'rotation': [],
     'zoom': []
-  };
-
-  // Methods
-  /**
-   * Register a callback to an event
-   */
-  on = function (eventType, callback, scope) {
-    var listenerType;
-    scope = scope || window;
-
-    if (!(eventType in events)) {
-      throw 'Impossible to bind event : ' + eventType;
-    }
-
-    listenerType = events[eventType];
-
-    listeners[listenerType].push({
-      'scope': scope,
-      'callback': callback
-    });
-  };
-
-  /**
-   * Unregister a previously registred callback to an event
-   * This do not works for anonymous callbacks
-   */
-  off = function (eventType, callback, scope) {
-    var listenerType, listenersList, indexOfCallbackInList, index;
-    scope = scope || window;
-
-    if (eventType && eventType in events) {
-      listenerType = events[eventType];
-
-      if (callback) {
-        listenersList = listeners[listenerType];
-        indexOfCallbackInList = function (list, callback, scope) {
-          var l = list.length - 1;
-          while (l >= 0) {
-            var item = list[l];
-            if (item.scope === scope && item.callback === callback) {
-              return l;
-            }
-            l = l-1;
-          }
-
-          return -1;
-        };
-        index = indexOfCallbackInList(listenersList, callback, scope);
-
-        if (index > (-1)) {
-          listenersList.splice(index, 1);
-        }
-      } else {
-        listeners[listenerType] = [];
-      }
-    } else {
-      listeners = {
-        'translation': [],
-        'rotation': [],
-        'zoom': []
-      };
-    }
   };
 
   /**
@@ -113,12 +55,151 @@ var CCNBridge = (function () {
     return events;
   };
 
-  // Public scope
+  // Methods
+  /**
+   * Register a callback for an event
+   */
+  on = function (eventType, callback, scope) {
+    var listenerType;
+    scope = scope || window;
+
+    // Check if the event-type is defined by this API
+    if (!(eventType in events)) {
+      throw 'Impossible to bind event : ' + eventType;
+    }
+
+    listenerType = events[eventType];
+
+    // Push a new listener that will be executed when this event will occur
+    listeners[listenerType].push({
+      'scope': scope,
+      'callback': callback
+    });
+  };
+
+  /**
+   * Alias for "on" method with rotate event-type
+   */
+  onRotate = function (callback, scope) {
+    this.on('rotate', callback, scope);
+  };
+
+  /**
+   * Alias for "on" method with zoom event-type
+   */
+  onZoom = function (callback, scope) {
+    this.on('zoom', callback, scope);
+  };
+
+  /**
+   * Unregister a previously registred callback for (an optionnal) event
+   * warning: This should not works for anonymous callbacks
+   */
+  off = function (eventType, callback, scope) {
+    var listenerType, listenersList, indexOfCallbackInList, index;
+    scope = scope || window;
+
+    // Check if the event-type is defined by this API
+    if (eventType && eventType in events) {
+      listenerType = events[eventType];
+
+      if (callback) {
+        // Get the current listeners list for the given event
+        listenersList = listeners[listenerType];
+        // Get the callback index in the list
+        indexOfCallbackInList = function (list, callback, scope) {
+          var l = list.length - 1;
+          while (l >= 0) {
+            var item = list[l];
+            // Compare both callback and scope
+            if (item.scope === scope && item.callback === callback) {
+              return l;
+            }
+            l = l-1;
+          }
+
+          return -1;
+        };
+        index = indexOfCallbackInList(listenersList, callback, scope);
+
+        // If the callback is found, remove it from the list
+        if (index > (-1)) {
+          listenersList.splice(index, 1);
+        }
+      } else {
+        // If the callback is not given, remove all callbacks of the event
+        listeners[listenerType] = [];
+      }
+    } else {
+      // If the event is not given, remove all callbacks of all events
+      listeners = {
+        'translation': [],
+        'rotation': [],
+        'zoom': []
+      };
+    }
+  };
+
+  /**
+   * Alias for "off" method with rotate event-type
+   */
+  offRotate = function (callback, scope) {
+    this.off('rotate', callback, scope);
+  };
+
+  /**
+   * Alias for "off" method with zoom event-type
+   */
+  offZoom = function (callback, scope) {
+    this.off('zoom', callback, scope);
+  };
+
+  /**
+   * Unregister all previously registred callbacks for (an optionnal) event
+   * warning: This should not works for anonymous callbacks
+   */
+  removeAllListeners = function (eventType) {
+    this.off(eventType);
+  };
+
+
+  /**
+   * Trigger method for C++ events
+   *
+   * Execute every callbacks registred for the given event
+   */
+  trigger = function (eventType, event) {
+    var listenerType, listenersList, l;
+
+    // Check if the event-type is defined by this API
+    if (!(eventType in events)) {
+      throw 'Impossible to trigger event : ' + eventType;
+    }
+
+    listenerType = events[eventType];
+    // Get the callbacks list
+    listenersList = listeners[listenerType];
+    l = listenersList.length - 1;
+
+    while (l >= 0) {
+      // For each callback, execute it with the given scope
+      listenersList[l].callback.apply(listenersList[l].scope, [event]);
+      l = l-1;
+    }
+  };
+
+  // Public scope : return all accessible methods
   return {
     'getEvents': getEvents,
     'getListeners': getListeners,
     'on': on,
-    'off': off
+    'onRotate': onRotate,
+    'onZoom': onZoom,
+    'off': off,
+    'offRotate': offRotate,
+    'offZoom': offZoom,
+    'removeAllListeners': removeAllListeners,
+    'trigger': trigger
   };
 })();
 
